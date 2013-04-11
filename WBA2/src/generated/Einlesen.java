25abb7a1f0be90f15a76f9a584a4018b2fd3dc11
Stefan Boergeling <steveboo@web.de>
2013-04-11 20:12:12 +0200

    Aufgabe 2 und 3 erweitert Aufgabe 4 b (Ausgabe noch erweitern) Ausgabe 4 rest fertig

--- WBA2/src/generated/Einlesen.java
+++ WBA2/src/generated/Einlesen.java
@@ -1,8 +1,20 @@
 package generated;
+import generated.Rezept.Kommentare.Kommentar;
+
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.datatype.Duration;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.namespace.QName;
 
 
 public class Einlesen {
@@ -16,7 +28,20 @@ public class Einlesen {
 		System.out.println(r.getZutaten().getZutat().get(1).getMenge());
 		System.out.println(r.getZubereitung().getArbeitszeit());
 		System.out.println(r.getPortionen().getAnzahl());
+		System.out.println(r.getKommentare().getKommentar().get(0).getValue());
+		Kommentar k = new Kommentar();
+		k.setAutor("max");
+		k.setDatum(r.getKommentare().getKommentar().get(0).getDatum());
+		k.setId("2");
+		k.setZeit(r.getKommentare().getKommentar().get(0).getZeit());
+		k.setValue("neuer kommentar");
 		
+		r.getKommentare().getKommentar().add(k);
+		System.out.println(r.getKommentare().getKommentar().get(1).getValue());
+		
+		Marshaller ma=context.createMarshaller();
+		ma.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		ma.marshal(r, new FileOutputStream("src/Aufgabe3Rezept.xml"));
 		
 	}
 
